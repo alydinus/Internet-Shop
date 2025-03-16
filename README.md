af41b929b72e2e04fda9ab10b995b0ae063c1185
Alydin <alykulovalydin@gmail.com>
2025-03-16 15:13:25 +0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Internet-Shop
-Project for midterm exam
---
+++ src/test/java/kg/spring/project/internet_shop/InternetShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.spring.project.internet_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
